527a7193aa3a781c11db158c7cd79b781b202435
Peter Lecki <peterlecki@gmail.com>
2013-05-29 18:41:31 -0700

    henry's tests removed from default.py and entered here instead

---
+++ controllers/admin.py
@@ -0,0 +1,8 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
+
+def getBlockcount():
+	bitcoind = AuthServiceProxy("https://bitcoinrpc:asdfasdf@127.0.0.1:8332")
+	blockcount = bitcoind.getblockcount()
+	return dict(message=T(str(blockcount)))
+	#return blockcount
+
