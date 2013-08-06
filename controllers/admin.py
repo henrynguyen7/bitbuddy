d94b47c0b4d13aab01c5ceb970e39468eddd71bf
Henry Nguyen <henry@dxconcept.com>
2013-08-06 00:25:43 -0700

    Continued implementation of bitcoinclient.py by adding "return None" placeholders in methods that have not yet been implemented. Fixed various syntax errors.

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,6 +1,10 @@
+# Runs all modules with the latest changes instead of cached version, useful for dev
+from gluon.custom_import import track_changes
+track_changes(True)
+
 from bitcoinclient import BitcoinClient
 
 def getBlockcount():
     client = BitcoinClient()
-    blockcount = client.get_blockcount()
+    blockcount = client.getblockcount()
     return dict(message=T(str(blockcount)))
--- modules/bitcoinclient.py
+++ modules/bitcoinclient.py
@@ -8,10 +8,6 @@ to a more secure client such as Armory.
 Author: Henry Nguyen
 """
 
-# Runs all modules with the latest changes instead of cached version, useful for dev
-from gluon.custom_import import track_changes
-track_changes(True)
-
 # TODO: Move bitcoind_resources.py into ../private and figure out how to import it into here
 # import sys
 # sys.path.append('../private/')
@@ -26,17 +22,17 @@ class BitcoinClient:
         """Initializes the class with the bitcoind client."""
         self.bitcoind = AuthServiceProxy(bitcoindProtocol + bitcoindUser + ":" + bitcoindPass + "@" + bitcoindIP + ":" + bitcoindPort)  
         
-    def addmultisigaddress(self, nrequired, ["key","key"], account=None):
-        """Add a nrequired-to-sign multisignature address to the wallet.
+    # def addmultisigaddress(self, nrequired, ["key","key"], account=None):
+    #     """Add a nrequired-to-sign multisignature address to the wallet.
         
-        Each key is a bitcoin address or hex-encoded public key. 
-        If [account] is specified, assign address to [account].
+    #     Each key is a bitcoin address or hex-encoded public key. 
+    #     If [account] is specified, assign address to [account].
         
-        nrequired: number of signatures to require
-        ["key","key"]: 
-        account: account to add to
-        """
-        return self.bitcoind.addmultisigaddress(nrequired, ["key","key"], account)
+    #     nrequired: number of signatures to require
+    #     ["key","key"]: 
+    #     account: account to add to
+    #     """
+    #     return self.bitcoind.addmultisigaddress(nrequired, ["key","key"], account)
     
     def addnode(self, node, action):
         """Attempts add or remove <node> from the addnode list or try a connection to <node> once.
@@ -54,46 +50,51 @@ class BitcoinClient:
         return self.bitcoind.backupwallet(destination)
     
     def createmultisig(self):
+        return None
     
     def createrawtransaction(self):
+        return None
     
     def decoderawtransaction(self):
+        return None
     
     def dumpprivkey(self, bitcoinaddress):
         """Reveals the private key corresponding to <bitcoinaddress>
         
         bitcoinaddress: address to reveal private key for
         """
-        self.bitcoind.dumpprivkey(bitcoinaddress)
+        return self.bitcoind.dumpprivkey(bitcoinaddress)
     
     def encryptwallet(self, passphrase):
         """Encrypts the wallet with <passphrase>.
         
         passphrase: passphrase to encrypt wallet with     
         """
-        self.bitcoind.encryptwallet(passphrase)
+        return self.bitcoind.encryptwallet(passphrase)
     
     def getaccount(self, bitcoinaddress):
         """Returns the account associated with the given address.
         
         bitcoinaddress: address associated with account
         """
-        self.bitcoind.getaccount(bitcoinaddress)
+        return self.bitcoind.getaccount(bitcoinaddress)
     
     def getaccountaddress(self, account):
         """Returns the current bitcoin address for receiving payments to this account.
         
         account: account to return receiving address for
         """
+        return self.bitcoind.getaccountaddress(account)
     
     def getaddednodeinfo(self):
+        return None
     
     def getaddressesbyaccount(self, account):
         """Returns the list of addresses for the given account.
         
         account: account to return addresses for        
         """
-        self.bitcoind.getaddressesbyaccount(account)
+        return self.bitcoind.getaddressesbyaccount(account)
     
     def getbalance(self, account=None, minconf=1):
         """Returns balance for server's total available balance.
@@ -101,14 +102,14 @@ class BitcoinClient:
         account: if [account] is not specified, returns the server's total available balance, otherwise returns the balance in the account.
         minconf: number of min confirmations to count
         """
-        self.bitcoind.getbalance(account, minconf)
+        return self.bitcoind.getbalance(account, minconf)
     
     def getblock(self, hash):
         """Returns information about the block with the given hash.
         
         hash: hash to return block info for
         """
-        self.bitcoind.getblock(hash)
+        return self.bitcoind.getblock(hash)
 
     def getblockcount(self):
         """Returns the number of blocks in the longest block chain."""
@@ -119,12 +120,14 @@ class BitcoinClient:
         
         index: index of block to hash
         """
+        return self.bitcoind.getblockhash(index)
     
     def getblocktemplate(self, params=None):
         """Returns data needed to construct a block to work on.
         
         params: 
         """
+        return None
     
     def getconnectioncount(self):
         """Returns the number of connections to other nodes."""
@@ -147,6 +150,7 @@ class BitcoinClient:
         return self.bitcoind.getinfo()
     
     def getmemorypool(self):
+        return None
     
     def getmininginfo(self):
         """ Returns an object containing mining-related information:
@@ -180,6 +184,7 @@ class BitcoinClient:
         return self.bitcoind.getrawmempool()
     
     def getrawtransaction(self):
+        return None
     
     def getreceivedbyaccount(self, account=None, minconf=1):
         """Returns the total amount received by addresses with [account] in transactions with at least [minconf] confirmations. 
@@ -187,6 +192,7 @@ class BitcoinClient:
         account: if [account] not provided return will include all transactions to all accounts
         minconf: minimum number of confirmations to count        
         """
+        return None
     
     def getreceivedbyaddress(self, bitcoinaddress, minconf=1):
         """Returns the total amount received by <bitcoinaddress> in transactions with at least [minconf] confirmations. 
@@ -197,6 +203,7 @@ class BitcoinClient:
         bitcoinaddress: address 
         minconf = minimum number of confirmations to count
         """
+        return self.bitcoind.getreceivedbyaddress(bitcoinaddress, minconf)
     
     def gettransaction(self, txid):
         """Returns an object about the given transaction containing:
@@ -213,21 +220,24 @@ class BitcoinClient:
             
         txid: transaction id
         """
+        return self.bitcoind.gettransaction(txid)
     
     def gettxout(self):
+        return None
     
     def gettxoutsetinfo(self):
         """Returns statistics about the unspent transaction output (UTXO) set."""
         return self.bitcoind.gettxoutsetinfo()
     
     def getwork(self):
+        return None
     
     def help(self, command=None):
         """List commands, or get help for a command.
         
         command: command to get help for
         """
-        self.bitcoind.help(command)
+        return self.bitcoind.help(command)
     
     def importprivkey(self, bitcoinprivkey, label, rescan=None):
         """Adds a private key (as returned by dumpprivkey) to your wallet. 
@@ -280,22 +290,22 @@ class BitcoinClient:
         """
         return self.bitcoind.listreceivedbyaddress(minconf, includeempty)
     
-    def listsinceblock(self, blockhash, target-confirmations):
+    def listsinceblock(self, blockhash, targetconfirmations):
         """Get all transactions in blocks since block [blockhash], or all transactions if omitted.
         
         blockhash:
         target-confirmations: 
         """
-        return self.bitcoind.listsinceblock(blockhash, target-confirmations)
+        return self.bitcoind.listsinceblock(blockhash, targetconfirmations)
     
-    def listtransactions(self, account, count=10, from=0):
+    def listtransactions(self, account, count=10, numfrom=0):
         """Returns up to [count] most recent transactions skipping the first [from] transactions for account [account]. 
         
         account: if [account] not provided will return recent transaction from all accounts
         count: 
         from: 
         """
-        return self.bitcoind.listtransactions(account, count, from)
+        return self.bitcoind.listtransactions(account, count, numfrom)
     
     def listunspent(self, minconf=1, maxconf=999999):
         """Returns array of unspent transaction inputs in the wallet.
@@ -303,13 +313,14 @@ class BitcoinClient:
         minconf: 
         maxconf: 
         """
-        return self.bitcoind.listunspent(account, count, from)
+        return self.bitcoind.listunspent(minconf, maxconf)
     
     def listlockunspent(self):
         """Returns list of temporarily unspendable outputs."""
         return self.bitcoind.listlockunspent()
     
     def lockunspent(self):
+        return None
     
     def move(self, fromaccount, toaccount, minconf=1, comment=None):
         """Move from one account in your wallet to another.
@@ -321,7 +332,7 @@ class BitcoinClient:
         """
         return self.bitcoind.move(fromaccount, toaccount, minconf, comment)
     
-    def sendfrom(self, fromaccount, tobitcoinaddress, amount, minconf=1, comment=None, comment-to=None):
+    def sendfrom(self, fromaccount, tobitcoinaddress, amount, minconf=1, comment=None, commentto=None):
         """Will send the given amount to the given address, ensuring the account has a valid balance using [minconf] confirmations. Returns the transaction ID if successful (not in JSON object).
         
         fromaccount: account to send from
@@ -331,13 +342,15 @@ class BitcoinClient:
         comment: optional comment
         comment-to: optional comment-to
         """
-        return self.bitcoind.sendfrom(fromaccount, tobitcoinaddress, amount, minconf, comment, comment-to)
+        return self.bitcoind.sendfrom(fromaccount, tobitcoinaddress, amount, minconf, comment, commentto)
     
     def sendmany(self):
+        return None
     
     def sendrawtransaction(self):
+        return None
     
-    def sendtoaddress(self, bitcoinaddress, amount, comment=None, comment-to=None):
+    def sendtoaddress(self, bitcoinaddress, amount, comment=None, commentto=None):
         """Sends bitcoins to an address. Returns the transaction ID <txid> if successful.
         
         bitcoinaddress: address to send to
@@ -345,7 +358,7 @@ class BitcoinClient:
         comment: optional comment
         comment-to: optional comment-to
         """
-        return self.bitcoind.sendtoaddress(bitcoinaddress, amount, comment, comment-to)
+        return self.bitcoind.sendtoaddress(bitcoinaddress, amount, comment, commentto)
     
     def setaccount(self, bitcoinaddress, account):
         """Sets the account associated with the given address.
@@ -358,13 +371,14 @@ class BitcoinClient:
         return self.bitcoind.setaccount(bitcoinaddress, account)
     
     def setgenerate(self):
-    
+        return None
+
     def settxfee(self, amount):
         """ Sets a transaction fee.
         
         amount: <amount> is a real and is rounded to the nearest 0.00000001
         """
-        return self.bitcoind.setaccount(amount)
+        return self.bitcoind.settxfee(amount)
     
     def signmessage(self, bitcoinaddress, message):
         """Sign a message with the private key of an address.
@@ -375,12 +389,14 @@ class BitcoinClient:
         return self.bitcoind.signmessage(bitcoinaddress, message)
     
     def signrawtransaction(self):
+        return None
     
     def stop(self):
         """Stop bitcoin server."""
         return self.bitcoind.stop()
     
     def submitblock(self):
+        return None
     
     def validateaddress(self, bitcoinaddress):
         """Return information about <bitcoinaddress>.
